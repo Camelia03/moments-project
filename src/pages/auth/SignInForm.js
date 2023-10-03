5843f5c76acb422d3d833776dea2075430425652
Camelia03 <camelia_weber@yahoo.com>
2023-10-03 15:56:38 +0000

    Challenge: Authentication: Handling Errors for SignInForm

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -22,6 +22,9 @@ function SignInForm() {
   });
   const { username, password } = signInData;
 
+  const [errors, setErrors] = useState({});
+
+
   const history = useHistory();
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -29,6 +32,8 @@ function SignInForm() {
       await axios.post("/dj-rest-auth/login/", signInData);
       history.push("/");
     } catch (err) {
+      setErrors(err.response?.data);
+
     }
   };
   const handleChange = (event) => {
@@ -54,6 +59,11 @@ function SignInForm() {
                         onChange={handleChange}
                     />
                 </Form.Group>
+                {errors.username?.map((message, idx) => (
+                  <Alert key={idx} variant="warning">
+                    {message}
+                  </Alert>
+                ))}
 
                 <Form.Group controlId="password">
                     <Form.Label className="d-none">Password</Form.Label>
@@ -66,12 +76,23 @@ function SignInForm() {
                         onChange={handleChange}
                     />
                 </Form.Group>
+                {errors.password?.map((message, idx) => (
+                  <Alert key={idx} variant="warning">
+                    {message}
+                  </Alert>
+                ))}
+
                 <Button 
                     className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
                     type="submit"
                 >
                     Sign In
                 </Button>
+                {errors.non_field_errors?.map((message, idx) => (
+                    <Alert key={idx} variant="warning" className="mt-3">
+                      {message}
+                    </Alert>
+                  ))}
             </Form>
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
