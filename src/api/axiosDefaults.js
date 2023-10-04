b78420e5626151626551494c4e64febcfdfa7171
Camelia03 <camelia_weber@yahoo.com>
2023-10-04 12:59:49 +0000

    Authentication: access and refresh tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://taskmanager-drf-b69d1e663d17.herokuapp.com/'
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.baseURL = "https://drf-api-rec.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
